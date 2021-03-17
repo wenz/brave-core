d13199de5988e7ad5e47a87f12f7177c29d2d9a9
zenparsing <zenparsing@gmail.com>
2021-03-17 11:14:00 -0400

    Implement changes to bat-native-ledger core classes based on feedback
    - Add template overload of AsyncResult::Then that maps the result value
    - Rename Task to Job
    - Use map instead of unordered_map
    - Use void* as job map key
    - Derive BATLedgerJob from SupportsWeakPtr
    - Disallow access to BATLedgerContext during construction and destruction
    - Add ContinueWith helper to BATLedgerJob

--- vendor/bat-native-ledger/src/bat/ledger/internal/core/async_result.h
+++ vendor/bat-native-ledger/src/bat/ledger/internal/core/async_result.h
@@ -43,10 +43,18 @@ class AsyncResult {
                                                      std::move(listener)));
   }
 
+  template <typename U>
+  AsyncResult<U> Then(base::OnceCallback<U(const T&)> map_complete) {
+    typename AsyncResult<U>::Resolver resolver;
+    Then(base::BindOnce(ThenCompleteCallback<U>, resolver,
+                        std::move(map_complete)));
+    return resolver.result();
+  }
+
   class Resolver {
    public:
     Resolver() {}
-    void Complete(T&& value) { result_.Complete(std::move(value)); }
+    void Complete(T value) { result_.Complete(std::move(value)); }
     AsyncResult result() const { return result_; }
 
    private:
@@ -72,9 +80,16 @@ class AsyncResult {
     std::list<Listener> listeners;
   };
 
-  void Complete(T&& value) {
+  void Complete(T value) {
     task_runner_->PostTask(
         FROM_HERE, base::BindOnce(SetCompleteInTask, store_, std::move(value)));
+  }
+
+  template <typename U>
+  static void ThenCompleteCallback(typename AsyncResult<U>::Resolver resolver,
+                                   base::OnceCallback<U(const T&)> map_complete,
+                                   const T& value) {
+    resolver.Complete(std::move(map_complete).Run(value));
   }
 
   static void AddListenerInTask(std::shared_ptr<Store> store,
--- vendor/bat-native-ledger/src/bat/ledger/internal/core/bat_ledger_job_unittest.cc
+++ vendor/bat-native-ledger/src/bat/ledger/internal/core/bat_ledger_job_unittest.cc
@@ -3,7 +3,7 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
-#include "bat/ledger/internal/core/bat_ledger_task.h"
+#include "bat/ledger/internal/core/bat_ledger_job.h"
 
 #include "base/bind.h"
 #include "base/memory/weak_ptr.h"
@@ -11,28 +11,23 @@
 
 namespace ledger {
 
-class BATLedgerTaskTest : public BATLedgerTest {};
+class BATLedgerJobTest : public BATLedgerTest {};
 
-TEST_F(BATLedgerTaskTest, StartTask) {
-  class Task : public BATLedgerTask<bool> {
+TEST_F(BATLedgerJobTest, StartJob) {
+  class Job : public BATLedgerJob<bool> {
    public:
-    explicit Task(BATLedgerContext* context) : BATLedgerTask<bool>(context) {}
-
     void Start(int n) {
-      AsyncResult<int>::Resolver resolver;
-      resolver.result().Then(
-          base::BindOnce(&Task::OnDone, weak_factory_.GetWeakPtr()));
-      resolver.Complete(std::move(n));
+      AsyncResult<int>::Resolver r;
+      r.result().Then(ContinueWith(&Job::OnDone));
+      r.Complete(n);
     }
 
    private:
     void OnDone(const int& n) { Complete(static_cast<bool>(n)); }
-
-    base::WeakPtrFactory<Task> weak_factory_{this};
   };
 
   bool value = false;
-  context()->StartTask<Task>(10).Then(
+  context()->StartJob<Job>(10).Then(
       base::BindLambdaForTesting([&value](const bool& v) { value = v; }));
 
   task_environment()->RunUntilIdle();
