0bda3854d2971d95a8c5613e1a120ceeffdc55b6
mkarolin <max@brave.com>
2021-04-02 10:59:49 -0400

    thread_pool.h replaced base::ThreadPool() as a task trait.
    Chromium change:
    
    https://chromium.googlesource.com/chromium/src/+/0d0d0a6a7f31704ca31756711fd5081fbb35e7f5
    
    commit 0d0d0a6a7f31704ca31756711fd5081fbb35e7f5
    Author: Javier Flores <javierrobles@chromium.org>
    Date:   Wed Mar 24 12:07:06 2021 +0000
    
        Reland "[base] Include task_traits.h from thread_pool.h"
    
        This reverts commit 6ab6376f157cf5945c518e484bf9f80f02f3dfc4.
    
        Reason for revert: Breakage was in internal code; relanding this change.
    
        Original change's description:
        > Revert "[base] Include task_traits.h from thread_pool.h"
        >
        > This reverts commit e5f980ff6bfe1f075e7c305e3381925121bb7551.
        >
        > Reason for revert: This breaks Chrome iOS
        >
        > Original change's description:
        > > [base] Include task_traits.h from thread_pool.h
        > >
        > > Migrate remaining straddlers.
        > >
        > > Note that base::PostJob() and blink::worker_pool:: migrated
        > > to base::ThreadPool internally already so callers specifying
        > > base::ThreadPool() on them as a trait were merely being redundant.
        > >
        > > Fix a few missing IWYU in //base not captured by
        > > https://chromium-review.googlesource.com/c/chromium/src/+/2775891
        > > which only migrated fully-specified base::ThreadPool:: usage.
        > >
        > > Remove constexpr from use_thread_pool(), it's now only
        > > relevant in a single scenario when posting tasks from Java via
        > > a private constructor.
        > >
        > > Bug: 1026641

--- components/speedreader/speedreader_rewriter_service.cc
+++ components/speedreader/speedreader_rewriter_service.cc
@@ -12,6 +12,7 @@
 #include "base/files/file_util.h"
 #include "base/logging.h"
 #include "base/task/post_task.h"
+#include "base/task/thread_pool.h"
 #include "brave/components/speedreader/speedreader_component.h"
 #include "brave/components/speedreader/speedreader_switches.h"
 #include "components/grit/brave_components_resources.h"
@@ -77,8 +78,8 @@ SpeedreaderRewriterService::~SpeedreaderRewriterService() {
 
 void SpeedreaderRewriterService::OnWhitelistReady(const base::FilePath& path) {
   VLOG(2) << "Whitelist ready at " << path;
-  base::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::ThreadPool(), base::MayBlock()},
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock()},
       base::BindOnce(
           &brave_component_updater::LoadDATFileData<speedreader::SpeedReader>,
           path),
@@ -87,8 +88,8 @@ void SpeedreaderRewriterService::OnWhitelistReady(const base::FilePath& path) {
 }
 
 void SpeedreaderRewriterService::OnStylesheetReady(const base::FilePath& path) {
-  base::PostTaskAndReplyWithResult(
-      FROM_HERE, {base::ThreadPool(), base::MayBlock()},
+  base::ThreadPool::PostTaskAndReplyWithResult(
+      FROM_HERE, {base::MayBlock()},
       base::BindOnce(&GetDistilledPageStylesheet, path),
       base::BindOnce(&SpeedreaderRewriterService::OnLoadStylesheet,
                      weak_factory_.GetWeakPtr()));
